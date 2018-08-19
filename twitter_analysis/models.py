abdb86c90280531830e89f6e4652ff19b4b49592
SinghSatbir <satbirform@gmail.com>
2018-08-19 23:54:53 +0530

    authentication

--- twitter_analysis/models.py
+++ twitter_analysis/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Word(models.Model):
+    keyword_entered=models.CharField(max_length=128)
+
+    def __str__(self):
+        return self.keyword_entered
--- twitter_analysis/views.py
+++ twitter_analysis/views.py
@@ -1,8 +1,15 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+import tweepy
 
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse('hello')
+    consumer_key = "6JMPol8gD9BuGUVKA5QQg5sBt"
+    consumer_secret = "TQTuy8wYA9zH9qTYO7TXukUNHpA49Q4gMpuhyTfEHs7TTddBqH"
+    access_token = "1029122549735673856-lXPMJE7Zr3uJ5kf4lUxVyMb65c0esk"
+    access_token_secret = "xpswIoqLfJlgWivb9vh7DtzdiCPrc1Uh7oETF6jOwTnSp"
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+
